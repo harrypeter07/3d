1b6d41953ae4fc0eb4f3a4cbf77b6221cfc7f2a4
Hassan Mansuri <146269350+harrypeter07@users.noreply.github.com>
2025-02-17 00:33:15 +0530

    Update loader.js

--- templates/loader.js
+++ templates/loader.js
@@ -1,1 +1,31 @@
+class Loader {
+    constructor() {
+        this.progress = 0;
+        this.total = 100;
+        this.progressBar = document.querySelector('.progress');
+        this.loaderElement = document.querySelector('.loader');
+    }
 
+    update(progress) {
+        this.progress = Math.min(progress, 100);
+        this.progressBar.style.width = `${this.progress}%`;
+
+        if (this.progress >= 100) {
+            setTimeout(() => {
+                this.hide();
+            }, 500);
+        }
+    }
+
+    hide() {
+        gsap.to(this.loaderElement, {
+            opacity: 0,
+            duration: 1,
+            onComplete: () => {
+                this.loaderElement.style.display = 'none';
+            }
+        });
+    }
+}
+
+const loader = new Loader();
